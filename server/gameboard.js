df85eb4eb3861b531e6d9577327a9122e3c59ff1
Chenchen Guo <gcc9108@gmail.com>
2016-07-26 19:02:13 -0700

    Change the source of the army detector. Now using our own implementation.

--- server/gameboard.js
+++ server/gameboard.js
@@ -18,22 +18,22 @@ var http = require('http');
 function init3Boards(size, board1, board2, board3) {
 
 
-    var i = 0;
-    var j = 0;
+	var i = 0;
+	var j = 0;
 
-    for(i=0;i<size;i++){
-        var temp1 = [];
-        var temp2 = [];
-        var temp3 = [];
-        for(j=0;j<size;j++){
-            temp1.push(0);
-            temp2.push(0);
-            temp3.push(0);
-        }
-        board1.push(temp1);
-        board2.push(temp2);
-        board3.push(temp3);
-    }
+	for(i=0;i<size;i++){
+		var temp1 = [];
+		var temp2 = [];
+		var temp3 = [];
+		for(j=0;j<size;j++){
+			temp1.push(0);
+			temp2.push(0);
+			temp3.push(0);
+		}
+		board1.push(temp1);
+		board2.push(temp2);
+		board3.push(temp3);
+	}
 
 }
 
@@ -60,11 +60,11 @@ function validateMoveAndCalculateCapturedTokens(prevBoard, currBoard, tempBoard,
 	 
 	//if position taken in currBoard
 	//	return -1
-    //CASE 1: position taken
-    if(currBoard[y][x] !== 0){
-        fn(-1);
-        return;
-    }
+	//CASE 1: position taken
+	if(currBoard[y][x] !== 0){
+		fn(-1);
+		return;
+	}
 
 	
 	//tempBoard = copy of board
@@ -76,25 +76,25 @@ function validateMoveAndCalculateCapturedTokens(prevBoard, currBoard, tempBoard,
 			}
 		}
 
-        makeMove(tempBoard,x,y,colour,lastMove,function(res){
-            console.log(res);
-            //CASE 2: suicidal
-            if(res.captured === 0){
-                if(suicide(tempBoard, x,y,colour)){
-                    //status = -3;
-                    fn(-3);
-                }else{
-                	fn(0);
-                }
-            //CASE 3: revert board
-            }else if(revertsGameBoard(tempBoard, prevBoard, x, y, colour)){
-                fn(-2);
-            //CASE 4: return captured tokens
-            }else{
-                fn(res.captured);
-            }
+		makeMove(tempBoard,x,y,colour,lastMove,function(res){
+			console.log(res);
+			//CASE 2: suicidal
+			if(res.captured === 0){
+				if(suicide(tempBoard, x,y,colour)){
+					//status = -3;
+					fn(-3);
+				}else{
+					fn(0);
+				}
+			//CASE 3: revert board
+			}else if(revertsGameBoard(tempBoard, prevBoard, x, y, colour)){
+				fn(-2);
+			//CASE 4: return captured tokens
+			}else{
+				fn(res.captured);
+			}
 
-        });
+		});
 	//if revertsBoard(tempBoard, prevBoard, x, y, colour)
 	//	return -2
 	//if suicide(tempBoard, x, y, colour)
@@ -115,11 +115,11 @@ function validateMoveAndCalculateCapturedTokens(prevBoard, currBoard, tempBoard,
  * @return {boolean} true if move would result in suicide 
  */
 function suicide(tempBoard, x, y, colour) {
-    var wouldBeCaptured = territoryDetector.getCapturedTokens(tempBoard);
-    if (colour === 1) {
-        return (wouldBeCaptured.capturedBlackArmyList.length > 0);
-    }
-    return (wouldBeCaptured.capturedWhiteArmyList.length > 0);
+	var wouldBeCaptured = territoryDetector.getCapturedTokens(tempBoard);
+	if (colour === 1) {
+		return (wouldBeCaptured.capturedBlackArmyList.length > 0);
+	}
+	return (wouldBeCaptured.capturedWhiteArmyList.length > 0);
 }
 
 /**
@@ -129,7 +129,7 @@ function suicide(tempBoard, x, y, colour) {
  * @return {Boolean} true if (x,y) is in the board
  */
 function inBounds(board, x, y) {
-    return (y >= 0 && y < board.length && x >= 0 && x < board.length);
+	return (y >= 0 && y < board.length && x >= 0 && x < board.length);
 }
 
 /**
@@ -145,16 +145,16 @@ function inBounds(board, x, y) {
  */
 function revertsGameBoard(tempBoard, prevBoard, x, y, colour) {
 	// if tempBoard is same as prevBoard, return true
-    var i=0;
-    var j=0;
-    for(i = 0;i< prevBoard.length;i++){
-        for(j = 0;j<prevBoard.length;j++){
-            if(tempBoard[i][j] !== prevBoard[i][j]){
-                return false;
-            }
-        }
-    }
-    return true;
+	var i=0;
+	var j=0;
+	for(i = 0;i< prevBoard.length;i++){
+		for(j = 0;j<prevBoard.length;j++){
+			if(tempBoard[i][j] !== prevBoard[i][j]){
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 /**
@@ -166,110 +166,39 @@ function revertsGameBoard(tempBoard, prevBoard, x, y, colour) {
  * @param colour {int} colour of token
  * @return captured tokens {int} 
  */
-function makeMove(board, y, x, colour, lastMove, fn) {
+function makeMove(board, x, y, colour, lastMove, fn) {
 
-    var captureCount = 0;
+	var returnObj = {
+		captured : 0,
+	};
 
-    if (colour !== 0) {
-
-        var option = {
-            // host: "roberts.seng.uvic.ca",
-            host: '127.0.0.1',
-            port: "30000",
-            path: "/util/findArmies",
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            }
-        }
-
-        var callback = function (response) {
-            var str = "";
-
-            response.on('data', function (chunk) {
-                str += chunk.toString();
-            });
-
-            response.on('end', function () {
-                //console.log("data: " + str);
-                var returnObj = {
-                    captured : 0,
-                    capturedTokens : [],
-                    board: [],
-                };
-                var jsonObj = JSON.parse(str);
-                console.log(jsonObj);
-                //add code here
-                var captureOccurred = false;
-                for (i = 0; i < jsonObj.armies.length; i++) {
-                    //if that armies has only one liberty AND color is different from armie
-                    if (jsonObj.armies[i].liberties.length === 1 && (jsonObj.armies[i].colour != colour)) {
-                        // if the only liberty is the place that the token is going to be placed
-                        if (jsonObj.armies[i].liberties[0][0] === x && jsonObj.armies[i].liberties[0][1] === y) {
-                            //place the token and capture this army
-                            board[x][y] = colour;
-                            
-                            for (var j = 0; j < jsonObj.armies[i].size; j++) {
-                                var tempx = jsonObj.armies[i].tokens[j].position[0];
-                                var tempy = jsonObj.armies[i].tokens[j].position[1];
-                                board[tempx][tempy] = 0;
-                                returnObj.captured++;
-                                returnObj.capturedTokens.push([tempy,tempx]);
-                            }
-                            captureOccurred = true;
-                        }
-                    }
-                }
-
-                if(!captureOccurred){
-                	// if capture didn't occur, simply place the token
-                	board[x][y] = colour;
-                }
-
-                returnObj.board = board;
-                //console.log("captured: " + captureCount);
-                fn(returnObj);
-            });
-
-
-        }
-
-        var tempLastMove = {
-        	x : lastMove.pass? 0: lastMove.y,
-        	y : lastMove.pass? 0: lastMove.x,
-        	c : lastMove.c,
-        	pass : lastMove.pass
-        };
-
-        postData = {
-            "board": board,
-            "size": board.length,
-            "last": tempLastMove
-        };
-
-        console.log(option);
-        console.log(postData);
-
-        var req = http.request(option, callback);
-
-        req.on('error', function (e) {
-            console.log("Error: " + e.message);
-        });
-
-        req.write(JSON.stringify(postData));
+	if (colour !== 0) {
+		board[y][x] = colour;
+		var capturedTokens = territoryDetector.getCapturedTokens(board);
+		var armyList = null;
+		if(colour == 1){   
+			// A black token has been placed
+			// Capture possible white tokens
+			armyList = capturedTokens.capturedWhiteArmyList;
+		}else{
+			armyList = capturedTokens.capturedBlackArmyList;
+		}
+		for(var i in armyList){
+			for(var j in armyList[i].listOfCoordinates){
+				var x = armyList[i].listOfCoordinates[j].x;
+				var y = armyList[i].listOfCoordinates[j].y;
+				board[y][x] = 0;
+				returnObj.captured++;
+			}
+		}
+		fn(returnObj);
+	}
 
 
 
-        req.end();
-
-
-    }
-
-
-
-    // place token onto board
-    // capture armies if applicable
-    // return number of captured tokens (could be 0)
+	// place token onto board
+	// capture armies if applicable
+	// return number of captured tokens (could be 0)
 }
 
 /**
